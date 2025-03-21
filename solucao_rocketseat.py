3ecf784e532bc854287c293ce5c45874c8517ba3
k-ccosta <k.accosta98@gmail.com>
2025-03-20 23:21:15 -0400

    feat: corrige validação de índice na função atualizar_nome_tarefa

--- solucao_rocketseat.py
+++ solucao_rocketseat.py
@@ -25,7 +25,7 @@ def ver_tarefas(tarefas):
 def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
     indice_tarefa_ajustado = int(indice_tarefa) - 1
 
-    if indice_tarefa_ajustado > 0 and indice_tarefa_ajustado < len(tarefas):
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
         tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
         print(f"Tarefa {indice_tarefa} atualizada para '{novo_nome_tarefa}'")
     else:
